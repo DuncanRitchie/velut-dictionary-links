bb10878ecef8bb6a7e94b7ed61552199667c2a1c
Duncan Ritchie <duncan.ritchie@informationcatalyst.com>
2021-09-04 21:19:54 +0100

    Links are created without diacritics/punctuation in input (for dictionaries other than velut)

--- main.js
+++ main.js
@@ -11,50 +11,63 @@ const link = document.getElementById("link");
 //// Sample data the user can load if they want to:
 const sampleData = "arma vir cano Troia primus ab os Italia fatum profugus Lavinia venio litus multus ille et terra iacto altus vis superus saevus memor Iuno ob ira";
 
-
 const dictionaries = [
     {
         "Dictionary": "Gaffiot",
-        "Formula": "https://micmap.org/dicfro/search/gaffiot/INPUT" 
+        "Formula": "https://micmap.org/dicfro/search/gaffiot/INPUT",
+        "RemoveDiacritics": true,
     },{
         "Dictionary": "Glosbe",
-        "Formula": "https://glosbe.com/la/en/INPUT"
+        "Formula": "https://glosbe.com/la/en/INPUT",
+        "RemoveDiacritics": true,
     },{
         "Dictionary": "LatDict",
-        "Formula": "https://www.latin-dictionary.net/search/latin/INPUT#search-results-list"
+        "Formula": "https://www.latin-dictionary.net/search/latin/INPUT#search-results-list",
+        "RemoveDiacritics": true,
     },{
         "Dictionary": "Liddell–Scott–Jones",
-        "Formula": "https://lsj.gr/wiki/INPUT"
+        "Formula": "https://lsj.gr/wiki/INPUT",
+        "RemoveDiacritics": true,
     },{
         "Dictionary": "Logeion",
-        "Formula": "https://logeion.uchicago.edu/INPUT"
+        "Formula": "https://logeion.uchicago.edu/INPUT",
+        "RemoveDiacritics": true,
     },{
         "Dictionary": "Morganianum",
-        "Formula": "https://neolatinlexicon.org/search/latin?search=INPUT"
+        "Formula": "https://neolatinlexicon.org/search/latin?search=INPUT",
+        "RemoveDiacritics": true,
     },{
         "Dictionary": "Numen",
-        "Formula": "https://latinlexicon.org/browse_latin.php?p1=INPUT&p2=1"
+        "Formula": "https://latinlexicon.org/browse_latin.php?p1=INPUT&p2=1",
+        "RemoveDiacritics": true,
     },{
         "Dictionary": "Packard Humanities",
-        "Formula": "https://latin.packhum.org/concordance?q=%23INPUT%23"
+        "Formula": "https://latin.packhum.org/concordance?q=%23INPUT%23",
+        "RemoveDiacritics": true,
     },{
         "Dictionary": "Perseus",
-        "Formula": "https://www.perseus.tufts.edu/hopper/searchresults?target=la&all_words=INPUT&all_words_expand=on&phrase=&any_words=&exclude_words=&documents="
+        "Formula": "https://www.perseus.tufts.edu/hopper/searchresults?target=la&all_words=INPUT&all_words_expand=on&phrase=&any_words=&exclude_words=&documents=",
+        "RemoveDiacritics": true,
     },{
         "Dictionary": "Thesaurus Linguae Latinae",
-        "Formula": "https://www.degruyter.com/databasecontent?dbf_0=tll-fulltext&dbid=tll&dbq_0=INPUT&dbsource=%2Fdb%2Ftll&dbt_0=fulltext&o_0=AND&sort=ttl-lemma-sort"
+        "Formula": "https://www.degruyter.com/databasecontent?dbf_0=tll-fulltext&dbid=tll&dbq_0=INPUT&dbsource=%2Fdb%2Ftll&dbt_0=fulltext&o_0=AND&sort=ttl-lemma-sort",
+        "RemoveDiacritics": true,
     },{
         "Dictionary": "Tower of Babel",
-        "Formula": "https://starling.rinet.ru/cgi-bin/response.cgi?root=config&morpho=0&basename=%5Cdata%5Cie%5Cpiet&first=1&text_lat=INPUT&method_lat=substring&ww_lat=on&ic_lat=on&text_any=&method_any=substring&sort=proto&ic_any=on"
+        "Formula": "https://starling.rinet.ru/cgi-bin/response.cgi?root=config&morpho=0&basename=%5Cdata%5Cie%5Cpiet&first=1&text_lat=INPUT&method_lat=substring&ww_lat=on&ic_lat=on&text_any=&method_any=substring&sort=proto&ic_any=on",
+        "RemoveDiacritics": true,
     },{
         "Dictionary": "velut",
-        "Formula": "https://www.velut.co.uk/INPUT"
+        "Formula": "https://www.velut.co.uk/INPUT",
+        "RemoveDiacritics": false,
     },{
         "Dictionary": "Whitaker’s Words",
-        "Formula": "https://archives.nd.edu/cgi-bin/wordz.pl?keyword=INPUT"
+        "Formula": "https://archives.nd.edu/cgi-bin/wordz.pl?keyword=INPUT",
+        "RemoveDiacritics": true,
     },{
         "Dictionary": "Wiktionary",
-        "Formula": "https://en.wiktionary.org/wiki/INPUT#Latin"
+        "Formula": "https://en.wiktionary.org/wiki/INPUT#Latin",
+        "RemoveDiacritics": true,
     }
 ]
 
@@ -148,6 +161,24 @@ const warnOfNoDictionariesSelected = () => {
     textByCreateLink.textContent = "No dictionaries selected!";
 }
 
+// Most online dictionaries need diacritics to be removed for searching.
+// (Eg, logeion.uchicago.edu/vocabulorum works
+// but logeion.uchicago.edu/vocābulōrum doesn’t.)
+// On velut, searching for (eg) "voca-bulo-rum" or "vocābulōrum" returns "vocābulōrum",
+// so this method will convert both to "vocabulorum".
+// Adapted from the /controllers folder of the velut codebase.
+const removeDiacritics = (macronizedWord) => {
+    // Macra are converted to hyphens (eg "ā" -> "a-").
+    let toHyphens = macronizedWord.replace(/\u0100/g,"A-").replace(/\u0101/g,"a-").replace(/\u0112/g,"E-").replace(/\u0113/g,"e-").replace(/\u012A/g,"I-").replace(/\u012B/g,"i-").replace(/\u014C/g,"O-").replace(/\u014D/g,"o-").replace(/\u016A/g,"U-").replace(/\u016B/g,"u-").replace(/\u1E7B/g,"u-:").replace(/\u0232/g,"Y-").replace(/\u0233/g,"y-").replace(/\u1e17/g,"e-.")
+    // Acutes are converted to dots (eg "á" -> "a.").
+    let toDots = toHyphens.replace(/\u00C1/g,"A.").replace(/\u00C9/g,"E.").replace(/\u00CD/g,"I.").replace(/\u00D3/g,"O.").replace(/\u00DA/g,"U.").replace(/\u00DD/g,"Y.").replace(/\u00E1/g,"a.").replace(/\u00E9/g,"e.").replace(/\u00ED/g,"i.").replace(/\u00F3/g,"o.").replace(/\u00FA/g,"u.").replace(/\u00FD/g,"y.")
+    // Diaereses are converted to cola (eg "ä" -> "a:").
+    const toCola = toDots.replace(/\u00E4/g,"a:").replace(/\u00EB/g,"e:").replace(/\u00CF/g,"i:").replace(/\u00EF/g,"i:").replace(/\u00F6/g,"o:").replace(/\u00FC/g,"u:").replace(/\u00FF/g,"y:")
+    // Remove all hyphens, dots, and cola.
+    return toCola.replace(/-/g,"").replace(/\./g,"").replace(/:/g,"")
+
+}
+
 const changeHrefOfLink = () => {
     if (currentIndexInUrls < urlObjects.length) {
         const currentUrlObject = urlObjects[currentIndexInUrls];
@@ -187,11 +218,15 @@ const createUrls = () => {
     const tickedDictionaries = getTickedDictionaries();
 
     words.forEach(word => {
+        const plainWord = removeDiacritics(word);
+
         tickedDictionaries.forEach(dictionary => {
+            const wordToSearchFor = dictionary.RemoveDiacritics ? plainWord : word;
+
             urlObjects.push({
                 Dictionary: dictionary.Dictionary,
-                Word: word,
-                URL: dictionary.Formula.replace("INPUT", word)
+                Word: wordToSearchFor,
+                URL: dictionary.Formula.replace("INPUT", wordToSearchFor),
             });
         });
     });
